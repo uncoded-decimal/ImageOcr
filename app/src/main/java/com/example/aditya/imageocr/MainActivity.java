4c2c585d1d3393798858b905b11a23a50fda818f
uncoded-decimal <30970288+uncoded-decimal@users.noreply.github.com>
2017-10-03 23:08:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/imageocr/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.aditya.imageocr;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    StringBuilder detectedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bitmap bmp= BitmapFactory.decodeResource(this.getResources(), R.drawable.rret);
+        detectedText= new StringBuilder();
+        gettingTextFromBitmap(bmp);
+
+        TextView textView = (TextView)findViewById(R.id.me);
+        textView.setText(detectedText);
+
+
+    }
+
+
+    private void gettingTextFromBitmap(Bitmap bitmap) {
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+        try {
+            if (!textRecognizer.isOperational()) {
+                new AlertDialog.
+                        Builder(this).
+                        setMessage("Text recognizer could not be set up on your device").show();
+                return;
+            }
+
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> origTextBlocks = textRecognizer.detect(frame);
+            List<TextBlock> textBlocks = new ArrayList<>();
+            for (int i = 0; i < origTextBlocks.size(); i++) {
+                TextBlock textBlock = origTextBlocks.valueAt(i);
+                textBlocks.add(textBlock);
+            }
+            Collections.sort(textBlocks, new Comparator<TextBlock>() {
+                @Override
+                public int compare(TextBlock o1, TextBlock o2) {
+                    int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
+                    int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
+                    if (diffOfTops != 0) {
+                        return diffOfTops;
+                    }
+                    return diffOfLefts;
+                }
+            });
+
+            for (TextBlock textBlock : textBlocks) {
+                if (textBlock != null && textBlock.getValue() != null) {
+                    detectedText.append(textBlock.getValue());
+                    detectedText.append("\n");
+                }
+            }
+
+        }  finally {
+        textRecognizer.release();
+    }
+    bitmap.recycle();
+
+}}
